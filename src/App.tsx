1f18560b2e96cfdda7251c6e5f1143ee25e12cc8
rajdangi31 <dangiprince263@gmail.com>
2025-05-18 20:34:39 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Navbar } from "./components/Navbar";
+import { ShareAPeekPage } from "./pages/ShareAPeekPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="bg-yellow-50 min-h-screen font-sans text-gray-800">
+        <Navbar />
+        <main className="max-w-5xl mx-auto px-4 pt-6">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shareApeek" element={<ShareAPeekPage />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/ShareAPeekPage.tsx
@@ -0,0 +1,9 @@
+import { SharePeek } from "../components/SharePeek";
+
+export const ShareAPeekPage = () => {
+    return ( <div>
+        <h2> Share a Peek!</h2>
+        <SharePeek />
+        </div>
+    );
+}
